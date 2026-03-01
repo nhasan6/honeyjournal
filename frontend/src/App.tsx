cebd248ba3ffcc156f38b33b8afef3b6ff201a45
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-03-01 10:12:41 -0500

    set up react router and create placeholders for pagess

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router'
+import Home from './pages/Home'
+import HoneyMap  from './pages/HoneyMap'
+import Admin from './pages/Admin'
 
 function App() {
 
   return (
-    "Hello World"
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/map" element={<HoneyMap />} />
+      <Route path="/admin" element={<Admin />} />
+
+
+    </Routes>
+  );
 }
 
 export default App
---
+++ frontend/src/pages/HoneyMap.tsx
@@ -0,0 +1,7 @@
+
+export default function HoneyMap() {
+  return (
+    <div>HoneyMap</div>
+  );
+}
+
